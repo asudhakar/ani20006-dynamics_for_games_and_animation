9bc2bd80bc95ec53bd909e838c64de98e51731d1
SWIN\102118468 <102118468>
2021-03-12 11:56:04 +1100

    UPDATE #2.8.3: Made code to calculate velocity from start and end positions

--- Lab 2/Assets/Scripts/MeteorMove.cs
+++ Lab 2/Assets/Scripts/MeteorMove.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class MeteorMove : MonoBehaviour
 {
-    public float speed;
+    public float force;
     public GameObject impactPrefab;
 
     private Rigidbody rb;
-    
+    private Animator anim;
+    private Vector3 startPos, endPos, velocity;
+    private bool isMoving;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (speed != 0 && rb != null)
-		{
-            rb.position += transform.forward * (speed * Time.deltaTime);
-		}
+        if (isMoving)
+            rb.AddForce(velocity * force);
     }
 
-	private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
 	{
-        speed = 0;
-
         ContactPoint contact = collision.contacts[0];
         Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
         Vector3 pos = contact.point;
@@ -37,7 +37,20 @@ public class MeteorMove : MonoBehaviour
             var impactVFX = Instantiate(impactPrefab, pos, rot) as GameObject;
             Destroy(impactVFX, 5);
         }
-
+        //Debug.Log(collision.gameObject.name);
         Destroy(gameObject);
 	}
+
+    public void GetStartPos()
+    {
+        startPos = transform.position;
+    }
+
+    public void GetEndPos()
+    {
+        endPos = transform.position;
+        velocity = endPos - startPos;
+        anim.enabled = false;
+        isMoving = true;
+    }
 }
